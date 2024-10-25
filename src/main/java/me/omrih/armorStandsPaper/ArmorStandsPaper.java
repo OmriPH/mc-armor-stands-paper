4c960c81be313edde78ae1898b59a7219a71d114
Omri <omri.hermon@gmail.com>
2024-10-25 22:33:49 +0300

    Init Plugin

---
+++ src/main/java/me/omrih/armorStandsPaper/ArmorStandsPaper.java
@@ -0,0 +1,17 @@
+package me.omrih.armorStandsPaper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ArmorStandsPaper extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
